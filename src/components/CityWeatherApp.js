353d654cbfb29a0713b895ec215f048cdcd8906a
Alexis Mata WebDev <alexedmata@gmail.com>
2023-01-18 16:58:43 -0400

    creado formulario citywether + peticion HTTP

--- src/components/CityWeatherApp.js
+++ src/components/CityWeatherApp.js
@@ -1,3 +1,41 @@
+import { useState } from "react"
+import CityWeatherForm from "./CityWeatherForm";
+
 export default function CityWeatherApp() {
-    return <div> - City Weather App - </div>
+    
+    const [weather, setWeather] = useState(null);
+    
+    /**
+     * Funcion asícrona creada para realizar una petición HTTP
+     * @param {*} city 
+     */
+    async function loadInfoWeather(city) {
+        try {
+            const request = await fetch(`${process.env.REACT_APP_API_URL}&key=${process.env.REACT_APP_API_KEY}&q=${city}`);
+            
+            const reqJson = await request.json();
+            setWeather(reqJson)
+            console.clear()
+            console.log(reqJson);
+        } catch (error) {
+            
+        }
+    }
+    /**
+     * Manejador del evento onChangeCity, al recibir el parametro city este irá
+     * ejecutando la funcion loadInfoWeather()
+     * @param {string} city 
+     */    
+    function handleChangeCity(city) {
+        setWeather(null);
+        loadInfoWeather(city);
+    }
+    return (
+        <div> 
+            <span className="appTitle">{process.env.REACT_APP_NAME}</span> 
+            <hr />
+            <CityWeatherForm onChangeCity={handleChangeCity} />
+            <div className="currentInfoCityWeather">Estado: {weather?.location.region} / Pais: {weather?.location.country}</div>
+        </div>
+    );
 }
---
+++ src/components/CityWeatherForm.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export default function CityWeatherForm({onChangeCity}) {
+     
+    const [city, setCity] = useState("Guayana City");
+        
+    /**
+     * Manejador del evento onChange del input el cual alamacena el valor de city.
+     * @param {*} e 
+     */
+    function handleChange(e) {
+        let value=e.target.value;
+        
+        if (value !== "") setCity(value);
+    }
+            
+    /**
+     * Manejador del evento onSubmit del formulario el cual envía el valor contenido en city.
+     * @param {*} e 
+     */
+    function handleSubmit(e) {
+        e.preventDefault();
+        onChangeCity(city);
+    }
+    return (
+        <form className="formCity" onSubmit={handleSubmit}>
+            <input className="inputCity" type="text" onChange={handleChange}  />
+        </form>
+    );
+    
+    
+}
